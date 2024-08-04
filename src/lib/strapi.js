cadca5f40b555b6544cbafbcb4f428d9c4fa0242
oussTheCOder <ouss.coder92@gmail.com>
2024-08-04 19:37:22 +0100

    fixing page size fetching wilayas

--- src/lib/strapi.js
+++ src/lib/strapi.js
@@ -46,3 +46,72 @@
   
     return data;
   }
+
+  // export default async function fetchApi({
+  //   endpoint,
+  //   query,
+  //   wrappedByKey,
+  //   wrappedByList,
+  // }) {
+  //   if (endpoint.startsWith('/')) {
+  //     endpoint = endpoint.slice(1);
+  //   }
+  
+  //   const url = new URL(`${import.meta.env.STRAPI_API_URL}/api/${endpoint}`);
+  //   console.log(url);
+  
+  //   if (query) {
+  //     Object.entries(query).forEach(([key, value]) => {
+  //       url.searchParams.append(key, value);
+  //     });
+  //   }
+  
+  //   let data = [];
+  //   let page = 1;
+  //   let pageSize = 100; // Default Strapi page size is 25
+  //   let total = 0;
+  //   let fetchedData;
+  
+  //   do {
+  //     url.searchParams.set('pagination[page]', page);
+  //     url.searchParams.set('pagination[pageSize]', pageSize);
+  
+  //     const res = await fetch(url.toString(), {
+  //       headers: {
+  //         'Content-Type': 'application/json; charset=utf-8',
+  //         'Cache-Control': 'no-cache',
+  //         'Pragma': 'no-cache',
+  //         'Expires': '0',
+  //       },
+  //     });
+  
+  //     fetchedData = await res.json();
+  //     console.log('Fetched Data:', fetchedData);
+  
+  //     if (wrappedByKey) {
+  //       fetchedData = fetchedData[wrappedByKey];
+  //     }
+  
+  //     if (fetchedData.data) {
+  //       data = data.concat(fetchedData.data);
+  //     }
+  
+  //     if (fetchedData.meta && fetchedData.meta.pagination) {
+  //       total = fetchedData.meta.pagination.total;
+  //     } else {
+  //       // If pagination info is not available, break the loop
+  //       console.warn('Pagination info not found, breaking the loop.');
+  //       break;
+  //     }
+  
+  //     page++;
+  //   } while (data.length < total);
+  
+  //   if (wrappedByList) {
+  //     data = data[0];
+  //   }
+  
+  //   return data;
+  // }
+  
+  
